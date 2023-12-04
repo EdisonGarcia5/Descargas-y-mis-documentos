0593657f536152c5c1897f10ee212676d6cc23bc
EdisonGarcía5 <edisongarcia542003@gmail.com>
2023-12-04 11:44:27 -0500

    first commit

---
+++ Empresa1.sql
@@ -0,0 +1,13 @@
+
+Create table cliente
+(
+  codigo int primary key,
+  Nombre varchar (50),
+  Apellido Varchar (50),
+  Direccion varchar (100)
+  
+)
+
+select * from cliente
+
+insert into cliente values('01','Juan','Perez','Calle gallina, N.123, M.4568, Q.Roo Mexíco');
